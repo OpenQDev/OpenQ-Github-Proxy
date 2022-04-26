52451999281691168e9ee04214d3fb030238f667
FlacoJones <andrew@openq.dev>
2022-04-26 07:57:14 -0500

    appends random PAT header

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
+
+let patsArray = process.env.PATS.split(',');
+
+function onProxyReq(proxyReq, req, res) {
+	let token = patsArray[Math.floor(Math.random() * patsArray.length)];
+	proxyReq.setHeader('Authorization', `Bearer ${token}`);
+}
 
 app.use('/', createProxyMiddleware({
 	target: 'https://api.github.com/graphql',
 	pathRewrite: { '^/': '' },
-	changeOrigin: true
+	changeOrigin: true,
+	onProxyReq
 }));
 
 app.listen(3000);
