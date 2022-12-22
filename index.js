e34358c6bf733ab701cef844cce1910e54895eb3
FlacoJones <andrew@openq.dev>
2022-12-22 17:17:51 -0500

    puts port in env

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ app.use('/', createProxyMiddleware({
   })
 }));
 
-app.listen(3000, () => {
-	console.log('Server started on port 3000')
+app.listen(process.env.PORT, () => {
+	console.log(`Server started on port ${process.env.PORT}`)
 })
