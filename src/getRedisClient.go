2324845da93cbc70caa20f77458395d6ec8bfdca
FlacoJones <andrew@openq.dev>
2023-01-16 16:30:29 -0600

    adds error for environment config and redis client

--- src/getRedisClient.go
+++ src/getRedisClient.go
@@ -19,12 +19,14 @@ func getRedisClient() *redis.Client {
 				MinVersion: tls.VersionTLS12,
 			},
 		})
-	} else {
+	} else if os.Getenv("DEPLOY_ENV") == "local" {
 		return redis.NewClient(&redis.Options{
 			Addr:     os.Getenv("REDIS_HOST") + ":" + os.Getenv("REDIS_PORT"),
 			Username: os.Getenv("REDIS_USERNAME"),
 			Password: os.Getenv("REDIS_PASSWORD"),
 			DB:       0,
 		})
+	} else {
+		panic("ENVIRONMENT NOT CONFIGURED - PLEASE SET DEPLOY_ENV to: local, test, staging or production")
 	}
 }
