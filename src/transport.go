a6dfa7f665c5e1751c05d0bb6dccfbe1f30feb35
FlacoJones <andrew@openq.dev>
2023-01-15 18:04:58 -0600

    moves transport to dedicated file

---
+++ src/transport.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+type transport struct {
+	http.RoundTripper
+}
+
+func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	const OAUTH_TOKEN_COOKIE_NAME string = "github_oauth_token_unsigned"
+
+	// Check for the "github_oauth_token_unsigned" cookie
+	if cookie, err := req.Cookie(OAUTH_TOKEN_COOKIE_NAME); err == nil {
+		// Add the cookie value as the Authorization header if present
+		req.Header.Set("Authorization", "Bearer "+cookie.Value)
+	} else {
+		// Add a default Authorization header if not present
+		commaDelimitedPATs := os.Getenv("PATS")
+		pats := strings.Split(commaDelimitedPATs, ",")
+		index := rand.Intn(len(pats))
+		randomPat := pats[index]
+
+		req.Header.Set("Authorization", "Bearer "+randomPat)
+	}
+
+	// Set the URL path to the GraphQL endpoint
+	req.URL.Scheme = "https"
+
+	req.URL.Path = "/graphql"
+
+	// Set the Host Header AND the URL Host to the GraphQL API endpoint (https://github.com/golang/go/issues/28168)
+	req.Host = "api.github.com"
+	req.URL.Host = "api.github.com"
+
+	reqBody, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	h := sha256.New()
+	h.Write([]byte(string(reqBody)))
+	cacheHex := h.Sum(nil)
+	cacheKey := hex.EncodeToString(cacheHex)
+
+	err = req.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	newBody := ioutil.NopCloser(bytes.NewReader(reqBody))
+	req.Body = newBody
+
+	// Make request
+	resp, err = t.RoundTripper.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	rerr := client.Set(req.Context(), cacheKey, b, 1*time.Hour).Err()
+	if rerr != nil {
+		panic(err)
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	// Reattach body to resp
+	body := ioutil.NopCloser(bytes.NewReader(b))
+	resp.Body = body
+
+	// Append CORS headers
+	resp.Header.Set("Access-Control-Allow-Origin", os.Getenv("ORIGIN"))
+	resp.Header.Set("Access-Control-Allow-Headers", "*")
+	resp.Header.Set("Access-Control-Allow-Credentials", "true")
+	resp.Header.Set("Access-Control-Allow-Methods", "POST")
+
+	resp.Header.Set("Content-Type", "application/json")
+	resp.Header.Set("Content-Encoding", "gzip")
+
+	return resp, nil
+}
