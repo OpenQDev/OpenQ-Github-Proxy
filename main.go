9d2bbb70dbabadeaa4d518c4e28b6625fc613045
FlacoJones <andrew@openq.dev>
2023-01-23 12:49:53 -0600

    removes caching

--- main.go
+++ main.go
@@ -150,46 +150,8 @@ func main() {
 			return
 		}
 
-		reqBody, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			panic(err)
-		}
-
-		h := sha256.New()
-		h.Write([]byte(string(reqBody)))
-		cacheHex := h.Sum(nil)
-		cacheKey := hex.EncodeToString(cacheHex)
-
-		err = r.Body.Close()
-		if err != nil {
-			panic(err)
-		}
-
-		newBody := ioutil.NopCloser(bytes.NewReader(reqBody))
-		r.Body = newBody
-
-		// Check if the response is in the cache
-		val, err := client.Get(r.Context(), cacheKey).Result()
-
-		if err == redis.Nil {
-			// Cache miss
-			fmt.Println("Cache miss. Calling Github GraphQL API")
-			proxy.ServeHTTP(w, r)
-		} else if err != nil {
-			// Error occurred while fetching from cache
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		} else {
-			fmt.Println("Cache hit! Sending cached response.")
-			// Response found in cache, serve it to the client
-			w.Header().Set("Access-Control-Allow-Origin", os.Getenv("ORIGIN"))
-			w.Header().Set("Access-Control-Allow-Headers", "*")
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Methods", "POST")
-
-			w.Header().Set("Content-Type", "application/json")
-			w.Header().Set("Content-Encoding", "gzip")
-			w.Write([]byte(val))
-		}
+		fmt.Println("Cache miss. Calling Github GraphQL API")
+		proxy.ServeHTTP(w, r)
 	})
 
 	// Use proxy for all calls on DefaultServerMux
